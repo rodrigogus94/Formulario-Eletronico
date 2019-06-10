19b73b73c34f45ccadd4ea926874b8bd62113122
rodrigo <rodrigo@rodrigo-Inspiron-3421>
2019-06-10 15:08:28 -0300

    Commit v1.5 Add verificação de login

--- src/java/br/upe/Controlador/ControladorUsuario.java
+++ src/java/br/upe/Controlador/ControladorUsuario.java
@@ -11,10 +11,14 @@ package br.upe.Controlador;
 
 import br.upe.Negocio.Usuario;
 import br.upe.Repositorio.RepositorioUsuario;
+import java.io.IOException;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.swing.JOptionPane;
+import org.primefaces.context.RequestContext;
 
 
 @ManagedBean(name = "ControladorUsuario")
@@ -64,6 +68,25 @@ public class ControladorUsuario {
         return this.ru.recuperarTodos();
     }
    
-    
+       
+
+    public String validarLogin(String login, String senha) {
+        
+        if (this.SelectUsuario.getLogin() != null) {
+
+            return "index.xhtml";
+        }
+        return null;
+    }
+
+  /*  public void logoff(){
+        this.logoff = null;
+        try{
+            FacesContext.getCurrentInstance().getExternalContext().redirect("../faces/login.xhtml");
+        }catch (IOException ex){
+            RequestContext.getCurrentInstance().showMessageInDialog
+        (new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario", ex.getMessage()));
+        }
+    }*/
   
 }
